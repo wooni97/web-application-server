5baae9d7f3b6df730ffe49ee9b52735cf128dcb7
wooni97 <jhw970714@gmail.com>
2024-03-06 19:04:42 +0900

    feat: JSESSIONID 발급 로직 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,8 +2,15 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Map;
+import java.util.UUID;
+
+import controller.Controller;
+import http.HttpRequest;
+import http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -19,17 +26,24 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
 
-            Controller controller = ControllerRepository.controller.get(request.getPath());
+            if (getJSESSIONID(request.getHeader("Cookie")) == null) {
+                response.addHeader("Set-Cookie",
+                        "JSEESIONID=" + UUID.randomUUID());
+            }
+
+            Controller controller = RequestMapping.getController(request.getPath());
 
             if (controller == null) {
                 String path = getDefaultPath(request.getPath());
                 response.forward(path);
-            } else {
-                controller.service(request, response);
+                return;
             }
+            controller.service(request, response);
+            
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -41,6 +55,12 @@ public class RequestHandler extends Thread {
         }
 
         return path;
+    }
+
+    private String getJSESSIONID(String cookieValue) {
+        Map<String, String> cookies =
+                HttpRequestUtils.parseCookies(cookieValue);
+        return cookies.get("JSESSIONID");
     }
 
 }
